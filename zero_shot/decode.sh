d55a6d8c61b59421b83e6986b787d309f782c4a1
Kun Qian <kun>
2022-08-03 16:26:28 -0400

    code for hf

---
+++ zero_shot/decode.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# 
+set -xue
+export PYTHONPATH=/home/qkun/projs/neurologic_decoding
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+DATA_DIR='../dataset'
+
+DEVICES=$1
+SPLIT=$2
+OUTPUT_FILE=$3
+
+# gpt2
+CUDA_VISIBLE_DEVICES=${DEVICES} python decode_gpt2.py --model_name 'gpt2-large' \
+  --output_file ${OUTPUT_FILE} \
+  --constraint_file ${DATA_DIR}/clean/constraint/${SPLIT}.constraint.json \
+  --batch_size 32 --beam_size 20 --max_tgt_length 32 --min_tgt_length 5 \
+  --ngram_size 3 --length_penalty 0.2 \
+  --prune_factor 500000 --sat_tolerance 2 --beta 1.25 --early_stop 10
---
+++ zero_shot/decode_pt.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DATA_DIR='../dataset'
+
+DEVICES=$1
+SPLIT=$2
+OUTPUT_FILE=$3
+
+# gpt2
+CUDA_VISIBLE_DEVICES=${DEVICES} python decode_pt.py --model_name 'gpt2-large' \
+  --output_file ${OUTPUT_FILE} \
+  --constraint_file ${DATA_DIR}/clean/constraint/${SPLIT}.constraint.json \
+  --input_path ${DATA_DIR}/clean/init/commongen.${SPLIT}.init.txt \
+  --batch_size 32 --beam_size 20 --max_tgt_length 32 --min_tgt_length 5 \
+  --ngram_size 3 --length_penalty 0.2 \
+  --prune_factor 500000 --sat_tolerance 2 --beta 1.25 --early_stop 10
